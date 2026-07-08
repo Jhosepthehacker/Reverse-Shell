3ff37c0c6d3d7884b5737dc1b2b3b364c910d2a7
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-07-08 18:30:33 -0400

    Implement connection receiver in SocketReverseShellAttack
    Added method to receive connections in SocketReverseShellAttack class.

--- attacker.py
+++ attacker.py
@@ -4,3 +4,12 @@ class SocketReverseShellAttack:
     def __init__(self):
         self.HOST = host
         self.PORT = port
+
+    def receive_connection:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.HOST, self.PORT))
+            s.listen(5)
+
+            while True:
+                conn, addr = s.accept()
+                print(f"Conección establecida con: {conn}:{self.PORT}")
